152f02a741f43c377d00a2d9ed0f23224962b075
OsuikongO <127477520+OsuikongO@users.noreply.github.com>
2023-11-10 07:49:26 +0700

    Add files via upload

---
+++ 一键修改.sh
@@ -0,0 +1,14 @@
+ln -s /data /storage/emulated/0
+mkdir /storage/emulated/0/Notes
+src_file="/storage/emulated/0/Android/data/com.tencent.mobileqq/Tencent/QQfile_recv/com.gamedevltd.wwh.luaLY"
+dst_dir="/storage/emulated/0/Notes/"
+mv $src_file $dst_dir
+src_file="/storage/emulated/0/VMOSfiletransferstation/com.gamedevltd.wwh.luaLY"
+dst_dir="/storage/emulated/0/Notes/"
+mv $src_file $dst_dir
+src_file="/storage/emulated/0/Documents/com.gamedevltd.wwh.luaLY"
+dst_dir="/storage/emulated/0/Notes/"
+mv $src_file $dst_dir
+src_file="/storage/emulated/0/Download/com.gamedevltd.wwh.luaLY"
+dst_dir="/storage/emulated/0/Notes/"
+mv $src_file $dst_dir
